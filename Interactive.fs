269e8bb37afc9a5b6edd8f6ff0505e1922bc8cc2
y2k <itwisterlx@gmail.com>
2017-06-07 21:55:59 +0300

    handle shell errors

--- Interactive.fs
+++ Interactive.fs
@@ -5,20 +5,23 @@ open System.Diagnostics
 open System.IO
 
 let execute (script: string) =
-    let tmpDir = Path.GetTempPath()
-    let tmp = Path.Combine(tmpDir, sprintf "tmp_%O.kts" (Guid.NewGuid()))
+    try
+        let tmpDir = Path.GetTempPath()
+        let tmp = Path.Combine(tmpDir, sprintf "tmp_%O.kts" (Guid.NewGuid()))
 
-    File.WriteAllText(tmp, script)
+        File.WriteAllText(tmp, script)
 
-    let psi = ProcessStartInfo()
-    psi.FileName <- "kotlinc"
-    psi.Arguments <- "-script " + tmp
-    psi.UseShellExecute <- false
-    psi.RedirectStandardOutput <- true
+        let psi = ProcessStartInfo()
+        psi.FileName <- "kotlinc"
+        psi.Arguments <- "-script " + tmp
+        psi.UseShellExecute <- false
+        psi.RedirectStandardOutput <- true
 
-    let p = Process.Start(psi)
-    let out = p.StandardOutput.ReadToEnd()
-    
-    File.Delete tmp
+        let p = Process.Start(psi)
+        let out = p.StandardOutput.ReadToEnd()
+        
+        File.Delete tmp
 
-    out
+        out
+    with
+    | e -> (string e)
